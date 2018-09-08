3e05d883166b04d2f0f62314f8d8d12f025ff147
JungJinHwan <yadot1478@naver.com>
2018-09-09 03:31:12 +0900

    Comment model, Like model 추가, tags 필드 추가

--- image/models.py
+++ image/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 from account.models import User
 from base.models import TimeStampedModel
@@ -12,3 +13,26 @@ class Image(TimeStampedModel):
     location = models.CharField(max_length=140)
     creator = models.ForeignKey(User, null=True, related_name='images', on_delete=models.PROTECT)
     caption = models.TextField()
+    tags = TaggableManager()
+
+
+class Comment(TimeStampedModel):
+
+    message = models.TextField()
+    creator = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
+    image = models.ForeignKey(Image, null=True, related_name='comments', on_delete=models.PROTECT)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return "{}".format(self.message)
+
+
+class Like(TimeStampedModel):
+
+    creator = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
+    image = models.ForeignKey(Image, null=True, related_name='likes', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "User: {} - Image Caption: {}".format(self.creator.username, self.image.caption)
