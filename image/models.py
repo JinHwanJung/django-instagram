dd9b7fe2e242602f3486babc80446d5fdc5277c6
JungJinHwan <yadot1478@naver.com>
2018-09-09 03:22:22 +0900

    Image model 추가

--- image/models.py
+++ image/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+from base.models import TimeStampedModel
+
+
+class Image(TimeStampedModel):
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    creator = models.ForeignKey(User, null=True, related_name='images', on_delete=models.PROTECT)
+    caption = models.TextField()
