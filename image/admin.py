decbf22350536ef436ae58365f8a3386d946ef2d
JungJinHwan <yadot1478@naver.com>
2018-09-09 03:39:51 +0900

    image 앱 어드민 항목 추가

--- image/admin.py
+++ image/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Like, Comment, Image
 
-# Register your models here.
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'creator',
+        'image',
+        'created_date',
+        'modified_date',
+    )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'message',
+        'creator',
+        'image',
+        'created_date',
+        'modified_date',
+    )
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'location',
+        'caption',
+        'file',
+        'creator',
+        'created_date',
+        'modified_date',
+    )
+
+    list_display_links = ('location', 'caption', )
+
+    search_fields = ('location', 'caption', )
+
+    list_filter = ('location', 'creator', )
