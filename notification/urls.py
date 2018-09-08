5dab80dae736afb108f7c39bba337bb6e30261cb
JungJinHwan <yadot1478@naver.com>
2018-09-09 04:11:44 +0900

    restore notifiacatoin views.py urls.py

--- notification/urls.py
+++ notification/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+
+from notification.views import Notifications
+
 app_name = "notification"
 
 urlpatterns = [
+    path(r'^$', Notifications.as_view(), name='notifications'),
 ]
--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from notification.models import Notification
+from notification.serializers import NotificationSerializer
+
+
+class Notifications(APIView):
+    def get(self, request):
+        user = request.user
+        notifications = Notification.objects.filter(to=user)
+        serializer = NotificationSerializer(notifications, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
