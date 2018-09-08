6b9616bf4208c2426feda03a89b75ecbd3fc650a
JungJinHwan <yadot1478@naver.com>
2018-09-09 01:45:24 +0900

    user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('not-specified', 'Not specified')
+    )
+
+    # First Name and Last Name do not cover name patterns
+    # around the globe.
+    name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    website = models.URLField(null=True)
+    bio = models.TextField(null=True)
+    phone = models.CharField(max_length=140, null=True)
+    gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+    following = models.ManyToManyField("self")
+    follower = models.ManyToManyField("self")
+    profile_image = models.ImageField(null=True)
