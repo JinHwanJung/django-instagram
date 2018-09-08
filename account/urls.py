dab8675249ec66fc99aae1c3c91ef806277a7989
JungJinHwan <yadot1478@naver.com>
2018-09-09 04:02:14 +0900

    restore urls.py, views.py

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,17 @@
+from django.urls import path
+
+from account.views import ExploreUsers, FollowUser, UnFollowUser, Search, UserProfile, UserFollowers, UserFollowings, \
+    ChangePassword
+
 app_name = "account"
 
 urlpatterns = [
+    path('explore/$', ExploreUsers.as_view(), name='explore_users'),
+    path('<int:user_id>/follow/', FollowUser.as_view(), name='follow_user'),
+    path('<int:user_id>/unfollow/', UnFollowUser.as_view(), name='unfollow_user'),
+    path('search/', Search.as_view(), name='search_user'),
+    path('<str:username>/', UserProfile.as_view(), name='user_profile'),
+    path('<str:username>/followers/', UserFollowers.as_view(), name='user_followers'),
+    path('<str:username>/followings/', UserFollowings.as_view(), name='user_followings'),
+    path('<str:username>/password/', ChangePassword.as_view(), name='change_password')
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,105 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from account.models import User
+from account.serializers import ListUserSerializer, UserProfileSerializer
+
+
+class ExploreUsers(APIView):
+    def get(self, request):
+        last_five_users = User.objects.order_by('-date_joined')[:5]
+        serializer = ListUserSerializer(last_five_users, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class FollowUser(APIView):
+    def post(self, request, user_id):
+        user = request.user
+        try:
+            found_user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        user.following.add(found_user)
+        found_user.new_follow_notify(user)
+        return Response(status=status.HTTP_200_OK)
+
+
+class UnFollowUser(APIView):
+    def post(self, request, user_id):
+        user = request.user
+        try:
+            found_user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        user.following.remove(found_user)
+        return Response(status=status.HTTP_200_OK)
+
+
+class UserProfile(APIView):
+    def get_user(self, username):
+        try:
+            found_user = User.objects.get(username=username)
+            return found_user
+        except User.DoesNotExist:
+            return None
+
+    def get(self, request, username):
+        found_user = self.get_user(username)
+        if not found_user:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = UserProfileSerializer(found_user)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, username):
+        found_user = self.get_user(username)
+        if not found_user:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        elif found_user != request.user:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            serializer = UserProfileSerializer(found_user, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserFollowers(APIView):
+    def get(self, request, username):
+        try:
+            found_user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        user_followers = found_user.follower.all()
+        serializer = ListUserSerializer(user_followers, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class UserFollowings(APIView):
+    def get(self, request, username):
+        try:
+            found_user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        user_followings = found_user.following.all()
+        serializer = ListUserSerializer(user_followings, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class Search(APIView):
+    def get(self, request):
+        username = request.query_params.get("username", None)
+        if username is not None:
+            users = User.objects.filter(username__icontains=username)
+            serializer = ListUserSerializer(users, many=True)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status.HTTP_400_BAD_REQUEST)
+
+
+class ChangePassword(APIView):
+    def put(self, request, username):
+        # 구현중...
+        pass
