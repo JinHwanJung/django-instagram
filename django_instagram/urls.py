d41abcfa4588d13afcf0243a777d1278eae9cee8
JungJinHwan <yadot1478@naver.com>
2018-09-30 20:15:52 +0900

    페이스북 소셜로그인 연동 완료

--- django_instagram/urls.py
+++ django_instagram/urls.py
@@ -18,13 +18,27 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from django_instagram.views import FacebookLogin, FacebookConnect
+from rest_auth.registration.views import SocialAccountListView, SocialAccountDisconnectView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('image/', include('image.urls', namespace='image')),
     path('notification/', include('notification.urls', namespace='notification')),
-    path('rest-auth/', include('rest_auth.urls')),
+
+    # rest-auth
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+
+    # socialaccounts
+    path('socialaccounts/', SocialAccountListView.as_view(), name='social_account_list'),
+    path('socialaccounts/facebook_login/', FacebookLogin.as_view(), name='fb_login'),
+    path('socialaccounts/facebook/connect/', FacebookConnect.as_view(), name='fb_connect'),
+    path('socialaccounts/<int:pk>/disconnect/', SocialAccountDisconnectView.as_view(),
+         name='social_account_disconnect'),
+    path('socialaccounts/', include('allauth.socialaccount.urls')),
 ]
+
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
